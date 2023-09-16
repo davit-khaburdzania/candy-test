560a065479dff05458459f3e138577b5ba1c8f3e
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2023-09-16 15:21:07 +0400

    removed key

--- config/initializers/initializers/openai.rb
+++ config/initializers/initializers/openai.rb
@@ -1,3 +1,3 @@
 OpenAI.configure do |config|
-  config.access_token = ENV['OPENAI_ACCESS_TOKEN'] || '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'
+  config.access_token = ENV['OPENAI_ACCESS_TOKEN']
 end
